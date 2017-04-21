21a7f182bfa88f49815a1ee7eec6b9c938432d5b
yevhen_reksilier <yevhen_reksilier@epam.com>
2017-04-21 13:34:48 +0300

    test

--- WebRegexUtils/src/main/java/reksilier/regexputil/service/ReplacerService.java
+++ WebRegexUtils/src/main/java/reksilier/regexputil/service/ReplacerService.java
@@ -3,7 +3,7 @@ package reksilier.regexputil.service;
 import reksilier.regexputil.service.replace.ReplacementResolver;
 
 public interface ReplacerService {
-	
+	   
 	String replaceSubStringsInText(String text, String replacingPart, ReplacementResolver replacementResolver);
 	
 }
